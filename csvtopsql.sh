4dfc07b25a0cadd4464763b92f66cd8c63db5c65
Neekow <45294301+Neek0w@users.noreply.github.com>
2022-05-25 10:13:16 +0200

    Add files via upload

--- csvtopsql.sh
+++ csvtopsql.sh
@@ -1,14 +1,12 @@
 fname="$1"
 sed 's/\s*,*\s*$//g' "$fname" > tmp.csv
 op=$(echo "$fname" | cut -d"." -f 1)
-opfile="$op.sql"
-op="'$op'"
-columns=$(head --lines=1 tmp.csv | sed s/,/\',\'/g | tr -d "\r\n")
-columns="'$columns'"
+opfile="sae.sql"
+op="$op"
+columns=$(head --lines=1 tmp.csv | tr -d "\r\n")
 tail --lines=+2 tmp.csv | while read l ; do
-values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
-values="\"$values\""
+values=$(echo $l | sed s/,/\',\'/g | tr -d "\r\n")
+values="'$values'"
 echo "INSERT INTO $op($columns) VALUES ($values);"
-done > "$opfile"
-gedit $opfile
+done >> "$opfile"
 rm tmp.csv
