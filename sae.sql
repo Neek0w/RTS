e9964dcd372aa292d047f7b65723ff7b2c92041f
nehol33 <95410575+nehol33@users.noreply.github.com>
2022-05-25 10:43:46 +0200

    Add files via upload

--- sae.sql
+++ sae.sql
@@ -1,13 +1,17 @@
-DO $$ DECLARE
-  r RECORD;
-BEGIN
-  FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-    EXECUTE 'DROP TABLE ' || quote_ident(r.tablename) || ' CASCADE';
-  END LOOP;
-END $$;
+DROP TABLE IF EXISTS Donneur CASCADE ;
+DROP TABLE IF EXISTS CentreDeCollecte CASCADE ;
+DROP TABLE IF EXISTS Laboratoire CASCADE ;
+DROP TABLE IF EXISTS Hopital CASCADE ;
+DROP TABLE IF EXISTS Receveur CASCADE ;
+DROP TABLE IF EXISTS Prix CASCADE ;
+DROP TABLE IF EXISTS TypeSang CASCADE ;
+DROP TABLE IF EXISTS PocheDisponible CASCADE ;
+DROP TABLE IF EXISTS Donneur_CentreCollecte CASCADE ;
+DROP TABLE IF EXISTS Centre_Hopital CASCADE ;
+DROP TABLE IF EXISTS Hopital_Receveur CASCADE ;
 
-CREATE TABLE donneur (
-    idDonneur SERIAL PRIMARY KEY,
+CREATE TABLE Donneur (
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(50),
     prenom VARCHAR(50),
     age INTEGER,
@@ -17,29 +21,29 @@ CREATE TABLE donneur (
     maladie VARCHAR(5)
 );
 
-CREATE TABLE centreDeCollecte (
-    idPoche SERIAL PRIMARY KEY,
+CREATE TABLE CentreDeCollecte (
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(50),
     adresse VARCHAR(50),
     telFixeCC VARCHAR(50)
 );
 
-CREATE TABLE laboratoire (
-    idLab SERIAL PRIMARY KEY,
+CREATE TABLE Laboratoire (
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(50),
     adresse VARCHAR(50),
     telFixeLab VARCHAR(50)
 );
 
-CREATE TABLE hopital (
-    idHop SERIAL PRIMARY KEY,
+CREATE TABLE Hopital (
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(50),
     adresse VARCHAR(50),
     telFixeHop VARCHAR(50)
 );
 
-CREATE TABLE receveur (
-    idReceveur SERIAL PRIMARY KEY,
+CREATE TABLE Receveur (
+    id SERIAL PRIMARY KEY,
     nom VARCHAR(50),
     prenom VARCHAR(50),
     age INTEGER,
@@ -48,30 +52,41 @@ CREATE TABLE receveur (
     groupeSanguin VARCHAR(50)
 );
 
-CREATE TABLE pocheDisponible (
-    idPoche SERIAL PRIMARY KEY,
+CREATE TABLE TypeSang (
+    id SERIAL PRIMARY KEY,
+    typesang VARCHAR(50)
+);
+
+CREATE TABLE Prix (
+    id SERIAL PRIMARY KEY,
+    prix FLOAT
+);
+
+CREATE TABLE PocheDisponible (
+    id SERIAL PRIMARY KEY,
     datePrelevement DATE,
     datePeremption DATE,
     typeSang VARCHAR(50),
     valider VARCHAR(5),
+    idTypeSang INTEGER REFERENCES typeSang,
+    idPrix INTEGER REFERENCES Prix,
     idLab INTEGER REFERENCES Laboratoire,
     idCentre INTEGER REFERENCES CentreDeCollecte,
     idDonneur INTEGER REFERENCES Donneur
 );
 
-CREATE TABLE donneur_CentreCollecte (
+CREATE TABLE Donneur_CentreCollecte (
     id_Donneur INTEGER REFERENCES Donneur,
     id_Centre INTEGER REFERENCES CentreDeCollecte
 );
 
-CREATE TABLE centre_Hopital (
+CREATE TABLE Centre_Hopital (
     id_Centre INTEGER REFERENCES CentreDeCollecte,
     id_Hopital INTEGER REFERENCES Hopital
 );
 
-CREATE TABLE receveur_Hopital (
-    id_Receveur INTEGER REFERENCES Receveur,
-    id_Hopital INTEGER REFERENCES Hopital
+CREATE TABLE Hopital_Receveur (
+    id_Hopital INTEGER REFERENCES Hopital,
+    id_Receveur INTEGER REFERENCES Receveur
 );
-
 
