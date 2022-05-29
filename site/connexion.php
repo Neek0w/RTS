51bd6106a9936b5cc79dbf1a53c079999aaa27bb
Neekow <45294301+Neek0w@users.noreply.github.com>
2022-05-29 18:06:50 +0200

    Add files via upload

--- site/connexion.php
+++ site/connexion.php
@@ -1,22 +1,21 @@
 <?php
 //Création des variables
-define("SERVEUR","localhost");
-define("USER","lchenoll");
-define("MDP","lchenoll");
-define("BD","lchenoll");
+define("SERVEUR","db");
+define("USER","root");
+define("MDP","root");
+define("BD","tp4");
 
 // gestion de la connexion
-  try
-  {
-       $connexion= new PDO('mysql:host='.SERVEUR.';dbname='.BD, USER, MDP);
-      $connexion->exec("SET CHARACTER SET utf8");	//Gestion des accents
- }
-
-//gestion des erreurs 
-catch(Exception $e)
- {
-        echo 'Erreur : '.$e->getMessage().'<br />';
-       echo 'N° : '.$e->getCode();
+try
+{
+    $connexion= new PDO('mysql:host='.SERVEUR.';dbname='.BD, USER, MDP);
+    $connexion->exec("SET CHARACTER SET utf8");	//Gestion des accents
 }
 
+//gestion des erreurs
+catch(Exception $e)
+{
+    echo 'Erreur : '.$e->getMessage().'<br />';
+    echo 'N° : '.$e->getCode();
+}
 ?>
